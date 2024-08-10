fa820c34232a74cc3a5ff650a670cf0f7fe8eeb3
Greg Bailey <gregbailey617@gmail.com>
2024-08-10 19:57:53 -0400

    adding routes

--- App/src/main.jsx
+++ App/src/main.jsx
@@ -3,8 +3,39 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import ErrorPage from './pages/ErrorPage';
+import AboutMe from './pages/about-me.jsx';
+import Portfolio from './pages/portfolio.jsx';
+import Contact from './pages/contact.jsx';
+import Resume from './pages/resume.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <AboutMe />,
+      },
+      {
+        path: 'portfolio',
+        element: <ProfilePage />,
+      },
+      {
+        path: 'contact',
+        element: <Contact />,
+      },
+      {
+        path: 'resume',
+        element: <Resume />,
+      },
+    ],
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+  <RouterProvider router={router} />
+);
